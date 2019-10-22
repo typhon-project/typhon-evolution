4a9496e86e0db44aeae14705b0fc36ad2ef74bc7
Pol Benats <pbenats@winad.priv>
2019-10-22 16:51:46 +0200

    module backend compilation

--- rest-api/src/main/java/com/typhon/evolutiontool/controller/RestController.java
+++ rest-api/src/main/java/com/typhon/evolutiontool/controller/RestController.java
@@ -1,24 +1,22 @@
 package com.typhon.evolutiontool.controller;
 
-import com.typhon.evolutiontool.Message;
-import com.typhon.evolutiontool.exceptions.EvolutionOperationNotSupported;
-import com.typhon.evolutiontool.exceptions.InputParameterException;
-import com.typhon.evolutiontool.services.EvolutionToolFacade;
-import com.typhon.evolutiontool.utils.TyphonMLUtils;
-import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import typhonml.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.typhon.evolutiontool.Message;
+
+import main.java.com.typhon.evolutiontool.services.EvolutionToolFacade;
 
 
-@org.springframework.web.bind.annotation.RestController
 public class RestController {
 
     Logger logger = LoggerFactory.getLogger(RestController.class);
     private EvolutionToolFacade evolutionToolFacade;
-    private ModelMapper modelMapper = new ModelMapper();
 
 
     @Autowired
@@ -30,7 +28,7 @@ public class RestController {
     @ResponseBody
     public Message postSmo(@RequestParam("initialModelPath") String initialModelPath, @RequestParam("finalModelPath") String finalModelPath){
 //        SMOJsonImpl smo = modelMapper.map(smoDto, SMOJsonImpl.class);
-        Message message;
+        Message message = new Message("test");
        // TODO Call backend.EvolutionToolFacade . evolve 
         
         

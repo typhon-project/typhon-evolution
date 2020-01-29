7d26948c8f9196f21268782ecaa771dae8a9e410
pbenats <seGerazu>
2020-01-29 10:06:59 +0100

    Change relation cardinality change operator ready to use, but not yet implemented by typhonQL

--- backend/src/main/java/com/typhon/evolutiontool/handlers/relation/RelationChangeCardinalityHandler.java
+++ backend/src/main/java/com/typhon/evolutiontool/handlers/relation/RelationChangeCardinalityHandler.java
@@ -26,8 +26,16 @@ public class RelationChangeCardinalityHandler extends BaseHandler {
         if (containParameters(smo, Arrays.asList(ChangeOperatorParameter.RELATION, ChangeOperatorParameter.CARDINALITY))) {
             RelationDO relationDO = RelationDOFactory.buildInstance((Relation) smo.getInputParameter().get(ChangeOperatorParameter.RELATION), false);
             int cardinalityValue = (Integer) smo.getInputParameter().get(ChangeOperatorParameter.CARDINALITY);
+
+            //Typhon ML
             Model targetModel = typhonMLInterface.changeCardinalityInRelation(relationDO, CardinalityDO.get(cardinalityValue), model);
+            targetModel = typhonMLInterface.removeCurrentChangeOperator(targetModel);
+
+            //Typhon QL
+            //Upload the new XMI to the polystore
+            typhonQLInterface.uploadSchema(targetModel);
             typhonQLInterface.changeCardinalityInRelation(relationDO.getName(), relationDO.getSourceEntity().getName(), CardinalityDO.get(cardinalityValue));
+
             return targetModel;
         } else {
             throw new InputParameterException("Missing parameters. Needed [" + ChangeOperatorParameter.RELATION + ", " + ChangeOperatorParameter.RELATION_NAME + "]");

313b889b1b75f3e24c04b0ac9186c843e2ac29be
Loup Meurice <loup.meurice@unamur.be>
2020-07-08 15:31:54 +0200

    jersey-client dependency exlcluded from ac.york.typhon.analytics

--- analytics/src/resources/mvn.sh
+++ analytics/src/resources/mvn.sh
@@ -8,6 +8,5 @@ mvn install:install-file -Dfile=it.univaq.disim.typhonml_1.0.0.202005251732.jar 
 mvn install:install-file -Dfile=org.eclipse.emf.common_2.16.0.v20190528-0845.jar -DgroupId=org.eclipse.emf.common -DartifactId=org.eclipse.emf.common -Dversion=2.16.0.v20190528-0845 -Dpackaging=jar
 mvn install:install-file -Dfile=org.eclipse.emf.ecore_2.18.0.v20190528-0845.jar -DgroupId=org.eclipse.emf.ecore -DartifactId=org.eclipse.emf.ecore -Dversion=2.18.0.v20190528-0845 -Dpackaging=jar
 mvn install:install-file -Dfile=org.eclipse.emf.ecore.xmi_2.16.0.v20190528-0725.jar -DgroupId=org.eclipse.emf.ecore.xmi -DartifactId=org.eclipse.emf.ecore.xmi -Dversion=2.16.0.v20190528-0725 -Dpackaging=jar
-mvn install:install-file -Dfile=ac.york.typhon.analytics-0.0.1-SNAPSHOT.jar -DgroupId=typhon -DartifactId=ac.york.typhon.analytics -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar
 
 

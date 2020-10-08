827e993749e1ada4411ba6eea8369ec39a86914d
Loup Meurice <loup.meurice@unamur.be>
2020-10-08 14:43:45 +0200

    TyphonML api updated (v 1.0.0.202009150837)

--- analytics/src/resources/mvn.sh
+++ analytics/src/resources/mvn.sh
@@ -4,7 +4,7 @@ mvn install:install-file -Dfile=rascal-0.18.jar -DgroupId=rascal -DartifactId=ra
 mvn install:install-file -Dfile=jruby.jar -DgroupId=jruby -DartifactId=jruby -Dversion=1.0.0-SNAPSHOT -Dpackaging=jar
 mvn install:install-file -Dfile=jcommander.jar -DgroupId=jcommander -DartifactId=jcommander -Dversion=1.0.0-SNAPSHOT -Dpackaging=jar
 mvn install:install-file -Dfile=asciidoctor.jar -DgroupId=asciidoctor -DartifactId=asciidoctor -Dversion=1.0.0-SNAPSHOT -Dpackaging=jar
-mvn install:install-file -Dfile=it.univaq.disim.typhonml_1.0.0.202005251732.jar -DgroupId=it.univaq.disim.typhonml -DartifactId=it.univaq.disim.typhonml -Dversion=1.0.0.202005251732 -Dpackaging=jar
+mvn install:install-file -Dfile=it.univaq.disim.typhonml_1.0.0.202009150837.jar -DgroupId=it.univaq.disim.typhonml -DartifactId=it.univaq.disim.typhonml -Dversion=1.0.0.202009150837 -Dpackaging=jar
 mvn install:install-file -Dfile=org.eclipse.emf.common_2.16.0.v20190528-0845.jar -DgroupId=org.eclipse.emf.common -DartifactId=org.eclipse.emf.common -Dversion=2.16.0.v20190528-0845 -Dpackaging=jar
 mvn install:install-file -Dfile=org.eclipse.emf.ecore_2.18.0.v20190528-0845.jar -DgroupId=org.eclipse.emf.ecore -DartifactId=org.eclipse.emf.ecore -Dversion=2.18.0.v20190528-0845 -Dpackaging=jar
 mvn install:install-file -Dfile=org.eclipse.emf.ecore.xmi_2.16.0.v20190528-0725.jar -DgroupId=org.eclipse.emf.ecore.xmi -DartifactId=org.eclipse.emf.ecore.xmi -Dversion=2.16.0.v20190528-0725 -Dpackaging=jar

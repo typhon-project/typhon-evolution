df9acf48b782486789bfeec6d79c2761b70fcb8f
pbenats <seGerazu>
2020-10-16 11:25:54 +0200

    Added missing dependency : org.eclipse.equinox.common

--- backend/src/main/resources/libs/mvn.cmd
+++ backend/src/main/resources/libs/mvn.cmd
@@ -6,3 +6,4 @@ mvn install:install-file -Dfile=it.univaq.disim.typhon.acceleo_1.0.0.20201015095
 mvn install:install-file -Dfile=it.univaq.disim.typhonml.xtext_1.0.0.202010150957.jar -DgroupId=it.univaq.disim.typhonml -DartifactId=it.univaq.disim.typhonml.xtext -Dversion=1.0.0.202010150957 -Dpackaging=jar
 mvn install:install-file -Dfile=org.eclipse.xtext_2.19.0.jar -DgroupId=org.eclipse.xtext -DartifactId=org.eclipse.xtext -Dversion=2.19.0 -Dpackaging=jar
 mvn install:install-file -Dfile=org.eclipse.xtext.util_2.23.0.v20200831-0730.jar -DgroupId=org.eclipse.xtext -DartifactId=org.eclipse.xtext.util -Dversion=2.23.0.v20200831-0730 -Dpackaging=jar
+mvn install:install-file -Dfile=org.eclipse.equinox.common_3.13.0.v20200828-1034.jar -DgroupId=org.eclipse.equinox -DartifactId=org.eclipse.equinox.common -Dversion=3.13.0.v20200828-1034 -Dpackaging=jar
